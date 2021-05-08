449fc0b39b657963bda59b6b686a3567207051f5
Sona <sonali23.1293@gmail.com>
2021-05-08 19:57:08 -0400

    shop-input to numeric value

--- assets/js/game.js
+++ assets/js/game.js
@@ -174,21 +174,19 @@ var endGame = function() {
 // shop
 var shop = function(){
     // ask player what they'd like to do
-    var shopOptionPrompt = window.prompt( "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter your choice: 'REFILL', 'UPGRADE', or 'LEAVE'.");
+    var shopOptionPrompt = window.prompt( "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     switch (shopOptionPrompt){
-        case "REFILL": //new case
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
 
-        case "UPGRADE": //new case
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE": // new case
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
 
             // do nothing, so function will end
